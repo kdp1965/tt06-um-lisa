360c557333e54273d102b31a83ce6ac528503c87
pettitkd <pettitkd@gmail.com>
2024-04-06 12:34:30 -0700

    Replacing src/RAM32.v with actual MACRO for Skywater 130 (was a Xilinx RAM wrapper).

--- src/RAM32.v
+++ src/RAM32.v
@@ -1,23 +1,30 @@
+/// sta-blackbox
 
-module RAM32
-(
-   input wire CLK,
-   input wire [3:0] WE0,
-   input wire EN0,
-   input wire [4:0] A0,
-   input wire [31:0] Di0,
-   output wire [31:0] Do0
+`default_nettype none
+
+module RAM32 (
+`ifdef USE_POWER_PINS
+    input VPWR,
+    input VGND,
+`endif
+    input CLK,
+    input [3:0] WE0,
+    input EN0,
+    input [4:0] A0,
+    input [31:0] Di0,
+    output reg [31:0] Do0
 );
+  reg [31:0] RAM[31:0];
 
-  D_RAM32 d_ram32_i
-  (
-      .clka (CLK),
-      .wea  (WE0),
-      .ena  (EN0),
-      .addra (A0),
-      .dina  (Di0),
-      .douta (Do0)
-  );
+  always @(posedge CLK)
+    if (EN0) begin
+      Do0 <= RAM[A0];
+      if (WE0[0]) RAM[A0][7:0] <= Di0[7:0];
+      if (WE0[1]) RAM[A0][15:8] <= Di0[15:8];
+      if (WE0[2]) RAM[A0][23:16] <= Di0[23:16];
+      if (WE0[3]) RAM[A0][31:24] <= Di0[31:24];
+    end else begin
+      Do0 <= 32'b0;
+    end
 
 endmodule
-
